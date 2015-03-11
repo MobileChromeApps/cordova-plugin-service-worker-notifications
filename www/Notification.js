50ea6b4262fd66f94488a868289f437b6c1b0b6f
Idan Mintz <imintz@chromium.org>
2015-03-11 17:49:47 -0400

    Implemented sound and icon properties, and tested on android device

--- www/Notification.js
+++ www/Notification.js
@@ -1,63 +1,6 @@
 var exec = require('cordova/exec');
-//require('de.appplant.cordova.plugin.local-notification');
 
 var idRegistry = [];
-
-function Notification(title, options) {
-    if (title === undefined) {
-	throw new TypeError("Failed to construct 'Notification': 1 argument required, but only 0 present");
-    }
-    this.title = title;
-    this.onclick = null;
-    this.onshow = null;
-    this.onerror = null;
-    this.onclose = null;
-    this.dir = "auto";
-    this.lang = "";
-    this.body = "";
-    this.tag = "";
-    this.icon = "";
-    this.sticky = false;
-    if (options === undefined) {
-
-    } else {
-	this.dir = options.dir;
-	this.lang = options.lang;
-	this.body = options.body;
-	this.tag = options.tag;
-	this.icon = options.icon;
-	this.sticky = options.sticky;
-    }
-    var uuid = (new Date()).getTime();
-    idRegistry.push({ id : uuid,
-		      notification: this
-		    });
-    console.log(uuid);
-    cordova.plugins.notification.local.schedule({
-	id: uuid,
-	title: this.title,
-	text: this.body,
-	//every: 0,
-	//at: new Date(),
-	//badge: 0,
-	//sound: this.sound,
-	data: { thing:"myData" },
-	//icon: this.icon,
-	ongoing: this.sticky
-    });
-    return this;
-}
-
-Notification.permission = false;
-
-//Since hasPermission is asynchronous it cannot simply be turned into a getter for permission
-//As a result we use it to update the class variable when device is ready and when the user requests permission
-//Todo: Find a better solution
-var updatePermission = function() {
-    cordova.plugins.notification.local.hasPermission(function (granted) {
-	Notification.permission = granted;
-    });
-};
 
 var getNotificationById = function(id) {
     var toReturn;
@@ -81,16 +24,65 @@ var getIdByNotification = function(notification) {
     return toReturn;
 };
 
+var updatePermission = function() {
+    cordova.plugins.notification.local.hasPermission(function (granted) {
+	Notification.permission = granted;
+    });
+};
+
+function Notification(title, options) {
+    if (title === undefined) {
+	throw new TypeError("Failed to construct 'Notification': 1 argument required, but only 0 present");
+    }
+    this.title = title;
+    this.onclick = null;
+    this.onshow = null;
+    this.onerror = null;
+    this.onclose = null;
+    this.dir = "auto";
+    this.lang = "";
+    this.body = "";
+    this.tag = "";
+    this.icon = "";
+    this.sticky = false;
+    this.sound = "";
+    if (options !== undefined) {
+	this.dir = options.dir;
+	this.lang = options.lang;
+	this.body = options.body;
+	this.tag = options.tag;
+	this.icon = options.icon;
+	this.sticky = options.sticky;
+	this.sound = options.sound;
+    }
+    var id = (new Date()).getTime();
+    idRegistry.push({ id : id,
+		      notification: this
+		    });
+    cordova.plugins.notification.local.schedule({
+	id: id,
+	title: this.title,
+	text: this.body,
+	//every: 0,
+	//at: new Date(),
+	//badge: 0,
+	sound: this.sound,
+	data: { thing:"myData" },
+	icon: (this.addEventListener !== undefined ? this.icon : ""),
+	ongoing: this.sticky
+    });
+    updatePermission();
+    return this;
+}
+
+Notification.permission = false;
+
+
 Notification.requestPermission = function() {
     cordova.plugins.notification.local.registerPermission(function(granted) {
 	console.log("permission has been granted: " + granted);
+	Notification.permission = granted;
     });
-    updatePermission();
-};
-
-Notification.prototype.close = function() {
-    var id = getIdByNotification(this);
-    cordova.plugins.notification.local.cancel(id, function() {});
 };
 
 var ctor = function() {};
@@ -105,15 +97,27 @@ catch(e)
     console.log("This browser does not support EventTarget");
 }
 
+Notification.prototype.close = function() {
+    var id = getIdByNotification(this);
+    console.log(id);
+    cordova.plugins.notification.local.cancel(id, function() {console.log("Canceled");});
+};
+
 document.addEventListener('deviceready', function() {
     updatePermission();
     cordova.plugins.notification.local.on("cancel", function(registration) {
 	notification = getNotificationById(registration.id);
-	notification.onclose.call();
+	try{
+	    notification.onclose.call();
+	} catch(e) {
+	}
     });
     cordova.plugins.notification.local.on("click", function(registration) {
 	notification = getNotificationById(registration.id);
-	notification.onclick.call();
+	try {
+	    notification.onclick.call();
+	} catch(e) {
+	}
     });
     cordova.plugins.notification.local.on("trigger", function(registration) {
 	notification = getNotificationById(registration.id);
