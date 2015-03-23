13ad3280eb56eaffb7e8e3cd88be738e0f72511b
Idan Mintz <imintz@chromium.org>
2015-03-23 11:51:14 -0400

    Added licenses

--- www/sw_assets/notificationassets.js
+++ www/sw_assets/notificationassets.js
@@ -1,3 +1,22 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
 showNotification = function(title, options) {
     return new Promise(function(resolve, reject) {
 	try {
@@ -44,6 +63,7 @@ if (typeof cordova === 'undefined') {
     cordova.plugins.notification = {};
     cordova.plugins.notification.local = {};
 }
+
 cordova.plugins.notification.local.schedule = function(options, callback, scope) {
     var notifications = Array.isArray(options) ? options : [options];
     for (var i = 0; i < notifications.length; i++) {
@@ -89,10 +109,8 @@ CDVNotification_defaults = {
 
 CDVNotification_getValueFor = function(options) {
     var keys = Array.apply(null, arguments).slice(1);
-
     for (var i = 0; i < keys.length; i++) {
 	var key = keys[i];
-
 	if (options.hasOwnProperty(key)) {
 	    return options[key];
 	}
@@ -101,15 +119,12 @@ CDVNotification_getValueFor = function(options) {
 
 CDVNotification_mergeWithDefaults = function(options) {
     var defaults = CDVNotification_defaults;
-
     options.at   = CDVNotification_getValueFor(options, 'at', 'firstAt', 'date');
     options.text = CDVNotification_getValueFor(options, 'text', 'message');
     options.data = CDVNotification_getValueFor(options, 'data', 'json');
-
     if (options.at === undefined || options.at === null) {
         options.at = new Date();
     }
-
     for (var key in defaults) {
         if (options[key] === null || options[key] === undefined) {
             if (options.hasOwnProperty(key) && ['data','sound'].indexOf(key) > -1) {
@@ -119,13 +134,11 @@ CDVNotification_mergeWithDefaults = function(options) {
             }
         }
     }
-
     for (key in options) {
         if (!defaults.hasOwnProperty(key)) {
             delete options[key];
         }
     }
-
     return options;
 };
 
@@ -137,15 +150,12 @@ CDVNotification_convertProperties = function(options) {
             options.id = options.id.toString();
         }
     }
-
     if (options.title) {
         options.title = options.title.toString();
     }
-
     if (options.text) {
         options.text  = options.text.toString();
     }
-
     if (options.badge) {
         if (isNaN(options.badge)) {
             options.badge = CDVNotification_defaults.badge;
@@ -153,15 +163,12 @@ CDVNotification_convertProperties = function(options) {
             options.badge = Number(options.badge);
         }
     }
-
     if (typeof options.at == 'object') {
         options.at = Math.round(options.at.getTime()/1000);
     }
-
     if (typeof options.data == 'object') {
         options.data = JSON.stringify(options.data);
     }
-
     return options;
 };
 
